5f5cae0e45c000846567f59ef7d4990abf5b7dc1
Juliao F. <juliao.gf@gmail.com>
2023-10-25 17:30:08 -0300

    feat: Exemplo finalizado

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # DIO - Projeto: Abstraindo Formações da DIO Usando Orientação a Objetos com Kotlin
 
-...
+Exemplo simples de classes em Kotlin representando um sistema de cursos (formações) onde os usuários podem ser inscritos e desinscritos. Foi adicionado também um método para listar os usuários inscritos.
 
 Este projeto foi descrito em [Aprenda Kotlin Com Exemplos: Desafio de Projeto (Lab)](https://github.com/digitalinnovationone/aprenda-kotlin-com-exemplos-lab).
 
--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,80 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun inscrever(usuario: Usuario) {
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("Usuário ${usuario.nome} inscrito na Formação de $nome")
+        } else {
+            println("Erro: O usuário ${usuario.nome} já estava inscrito na Formação de $nome")
+        }
+    }
+
+    fun desinscrever(usuario: Usuario) {
+        if (inscritos.contains(usuario)) {
+            inscritos.remove(usuario)
+            println("Usuário ${usuario.nome} desinscrito da Formação de $nome")
+        } else {
+            println("Erro: O usuário ${usuario.nome} não está inscrito na Formação de $nome")
+        }
+    }
+
+    fun exibirUsuariosInscritos() {
+        println("Há ${inscritos.size} usuários inscritos na Formação de $nome (Nível: $nivel):")
+        for (i in 0 until inscritos.size) {
+            println("${i+1} - ${inscritos[i].nome}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudosFormacaoLinux = listOf<ConteudoEducacional>(
+        ConteudoEducacional("Terminal", 60),
+        ConteudoEducacional("Shell Script", 120),
+        ConteudoEducacional("Gerenciador de pacotes", 120)
+    )
+
+    val conteudosFormacaoKotlin = listOf<ConteudoEducacional>(
+        ConteudoEducacional("Variáveis", 60),
+        ConteudoEducacional("Classes", 120),
+        ConteudoEducacional("Coroutines", 120)
+    )
+
+    val formacao1 = Formacao("Linux", Nivel.BASICO , conteudosFormacaoLinux)
+    val formacao2 = Formacao("Kotlin", Nivel.INTERMEDIARIO, conteudosFormacaoKotlin)
+
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+    val usuario3 = Usuario("Pedro")
+    val usuario4 = Usuario("Joana")
+    val usuario5 = Usuario("Paulo")
+    val usuario6 = Usuario("Fernanda")
+
+    formacao1.inscrever(usuario1)
+    formacao1.inscrever(usuario2)
+    formacao1.inscrever(usuario1)
+    formacao1.desinscrever(usuario1)
+    println()
+    
+    formacao1.exibirUsuariosInscritos()
+    println()
+
+    formacao2.inscrever(usuario1)
+    formacao2.inscrever(usuario2)
+    formacao2.inscrever(usuario3)
+    formacao2.inscrever(usuario4)
+    formacao2.inscrever(usuario5)
+    formacao2.inscrever(usuario6)
+    println()
+
+    formacao2.exibirUsuariosInscritos()
 }
